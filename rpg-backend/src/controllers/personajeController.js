4465f94c26f327cbd80e987aa2cc21db15de4462
MARCO <jcmm.1994@gmail.com>
2026-04-28 16:21:02 +0100

    Estructuracion de carpetas

--- rpg-backend/src/controllers/personajeController.js
+++ rpg-backend/src/controllers/personajeController.js
@@ -9,7 +9,7 @@ const obtenerUno = (req, res, next) => {
   try { res.json(service.obtenerPorId(Number(req.params.id))) }
   catch (err) { next(err) }
 }
-
+ 
 const crearManual = (req, res, next) => {
   try { res.status(201).json(service.crearManual(req.body)) }
   catch (err) { next(err) }
